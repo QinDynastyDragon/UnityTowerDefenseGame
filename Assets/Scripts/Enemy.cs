20ae08f21ae8622254238579807484a6eb70b9a1
Jimmy Chan <jimhaotra1234@hotmail.com>
2017-04-04 09:32:59 -0700

    Variable adjustments of enemy speeds and hierachies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,23 +3,24 @@
 public class Enemy : MonoBehaviour 
 {
 
-    public float speed = 10f;
+    public float startSpeed = 10f;
 
-    public int health = 100;
+    [HideInInspector] // so the speed variable doesnt show up
+    public float speed;
 
-    public int value = 50;
+    public float health = 100;
+
+    public int worth = 50;
 
     public GameObject deathEffect;
 
-    private Transform target;
-    private int wavepointIndex = 0;
 
-    void Start ()
+    void Start()
     {
-        target = Waypoints.points[0]; // because points was static, we can use .points
+        speed = startSpeed;
     }
 
-    public void TakeDamage (int amount)
+    public void TakeDamage (float amount)
     {
         health -= amount;
 
@@ -29,42 +30,18 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public void Slow (float pct)
+    {
+        speed = startSpeed * (1f - pct);
+    }
+
     void Die()
     {
-        PlayerStats.Money += value;
+        PlayerStats.Money += worth;
 
         GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
 
-        Destroy(gameObject);
-    }
-
-    void Update ()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World); // Time.deltaTime, not depending on the framerate, even if fps drops
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-        {
-            GetNextWaypoint ();
-        }
-    }
-
-
-    void GetNextWaypoint ()
-    {
-        if (wavepointIndex >= Waypoints.points.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
-    }
-
-    void EndPath()
-    {
-        PlayerStats.Lives--;
         Destroy(gameObject);
     }
 
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+    private Enemy targetEnemy;
 
     [Header("General")]
 
@@ -16,6 +17,10 @@ public class Turret : MonoBehaviour
 
     [Header("Use Laser")]
     public bool useLaser = false;
+
+    public int damageOverTime = 30;
+    public float slowAmount = .5f;
+
     public LineRenderer lineRenderer;
     public ParticleSystem impactEffect;
     public Light impactLight;
@@ -55,6 +60,7 @@ public class Turret : MonoBehaviour
         if (nearestEnemy != null && shortestDistance <= range)
         {
             target = nearestEnemy.transform;
+            targetEnemy = nearestEnemy.GetComponent<Enemy>();
         }
         else
         {
@@ -108,6 +114,9 @@ public class Turret : MonoBehaviour
 
     void Laser()
     {
+        targetEnemy.TakeDamage(damageOverTime * Time.deltaTime);
+        targetEnemy.Slow(slowAmount);
+
         if (!lineRenderer.enabled)
         {
             lineRenderer.enabled = true;
