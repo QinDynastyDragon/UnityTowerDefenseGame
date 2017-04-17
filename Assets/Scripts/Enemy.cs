3c606788396b2844653569841f7297a535d56035
Jimmy Chan <jimhaotra1234@hotmail.com>
2017-04-17 01:05:29 -0700

    Main menu finalized with animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour 
 {
@@ -8,21 +9,28 @@ public class Enemy : MonoBehaviour
     [HideInInspector] // so the speed variable doesnt show up
     public float speed;
 
-    public float health = 100;
+    public float startHealth = 100;
+    private float health;
 
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
 
     void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage (float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if (health <= 0)
         {
